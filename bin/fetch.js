1af256b8d5e5838d5a55035b6b08cc3b72da208f
MichalCz <cz@signicode.com>
2019-09-01 10:46:24 +0000

    Fixes

--- bin/fetch.js
+++ bin/fetch.js
@@ -9,44 +9,47 @@ const {promisify} = require("util");
 
 const {get} = require("https");
 
-const awaitWritten = (stream, path) => new Promise(
-    (s, j) => stream
-        .pipe(fs.createWriteStream(resolve(__dirname, path)))
-        .on("finish", s).on("error", j)
-).then();
+const awaitWritten = (stream, path) => {
+    return new Promise(
+        (s, j) => stream
+            .pipe(fs.createWriteStream(resolve(__dirname, path)))
+            .on("finish", s).on("error", j)
+    );
+};
 
 (async () => {
     const out = await new Promise(
-        s => get("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat", (stream) => s(stream))
+        s => get("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat", (stream) => s(stream)).end()
     );
 
     const columns = [
         "airportid", "name", "city", "country", "iata", "icao", "latitude", "longitude", "altitude", "timezone", "dst", "tz", "type", "source"
     ];
 
-    /**
-     * @type {DataStream}
-     */
+    let i = 0; let j = 0;
     const stream = StringStream.from(out, {})
         .CSVParse()
-        .map(x => x.slice().map(col => col === "\\N" ? "" : col))
+        .each(() => j++)
+        .map(x => x.map(col => col === "\\N" ? "" : col))
         .map(x => {
             const out = {};
             for (let i = 0; i < columns.length; i++) out[columns[i]] = x[i];
 
             return out;
         })
-        .each(airport => promisify(fs.writeFile)(resolve(__dirname, `../dist/icaos/${airport.icao}.json`), JSON.stringify(airport), {flag:"w+"}))
+        .each(() => i++)
     ;
-
-    stream.tap();
 
     return Promise.all([
         awaitWritten(stream.pipe(new DataStream({}), {}).toJSONArray(), "../dist/array.json"),
-        awaitWritten(stream.pipe(new DataStream({}), {}).filter(item => item.iata).toJSONArray(["{\n","\n}"], ",\n", item => `"${item.iata}": "${item.icao}"`), "../dist/iata2icao.json"),
+        awaitWritten(stream.pipe(new DataStream({}), {}).filter(item => item.iata).toJSONArray(
+            // @ts-ignore
+            ["{\n","\n}"], ",\n", item => `"${item.iata}": "${item.icao}"`
+        ), "../dist/iata2icao.json"),
         awaitWritten(stream.pipe(new DataStream({}), {}).map(item => item.icao).toJSONArray(), "../dist/icaos.json"),
         awaitWritten(stream.pipe(new DataStream({}), {}).filter(item => item.iata).toJSONObject(item => item.iata), "../dist/iata.json"),
-        awaitWritten(stream.pipe(new DataStream({}), {}).filter(item => item.icao).toJSONObject(item => item.icao), "../dist/icao.json")
+        awaitWritten(stream.pipe(new DataStream({}), {}).filter(item => item.icao).toJSONObject(item => item.icao), "../dist/icao.json"),
+        stream.consume(airport => promisify(fs.writeFile)(resolve(__dirname, `../dist/icaos/${airport.icao}.json`), JSON.stringify(airport), {flag:"w+"}))
     ]);
 })()
     .then(
